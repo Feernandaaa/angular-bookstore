5fadbce2557546ece1eaec6746abde7d7bc25f08
Feernandaaa <nandapoderosa.mf@gmail.com>
2022-07-04 19:55:26 -0300

    finalizando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
+import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
+import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ProductItemComponent } from './components/bookstore-app/product-list/product-item/product-item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BooksService } from './components/bookstore-app/product-list/product-list.component.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BookstoreAppComponent,
+    FiltersComponent,
+    ProductListComponent,
+    NavComponent,
+    ProductItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+function AppRoutingModule(AppRoutingModule: any): void {
+  throw new Error('Function not implemented.');
+}
+
